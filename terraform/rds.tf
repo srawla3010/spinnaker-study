d604d0415fcf243800c1c2ed1e89bf8e3b0df9aa
srawla3010 <srawla@gmail.com>
2021-05-14 11:18:46 -0700

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -5,7 +5,7 @@ provider "aws" {
 variable "team-name" {
   description = "team-name"
   type        = string
-  default     = "default-team-name"
+  default     = "shyam-solo-name"
 }
 
 resource "aws_db_instance" "microservice-db" {
